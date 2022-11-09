1fcd7a2dca54c6774ce5b4eb548c75ec11b1da9e
JMJHOX <josemiguelaparicio507@gmail.com>
2022-11-09 00:09:16 -0500

    added dashboard

---
+++ src/data/fakedatatrans3.js
@@ -0,0 +1,89 @@
+
+import moment from "moment-timezone";
+
+export default [
+    {
+        "invoiceNumber": 300500,
+        "nameOrder":"Joh Doe4",
+        "status": "Paid",
+        "progress": "Dead",
+        "price": "799,00",
+        "issueDate": moment().subtract(1, "days").format("DD MMM YYYY"),
+        "dueDate": moment().subtract(1, "days").add(1, "month").format("DD MMM YYYY")
+    },
+    {
+        "invoiceNumber": 300499,
+        "nameOrder":"John Doe2",
+        "status": "Paid",
+        "progress": "Cooking",
+        "price": "799,00",
+        "issueDate": moment().subtract(2, "days").format("DD MMM YYYY"),
+        "dueDate": moment().subtract(2, "days").add(1, "month").format("DD MMM YYYY")
+    },
+    {
+        "invoiceNumber": 300498,
+        "nameOrder":"John Doe2",
+        "status": "Paid",
+        "progress": "Cooking",
+        "price": "799,00",
+        "issueDate": moment().subtract(2, "days").format("DD MMM YYYY"),
+        "dueDate": moment().subtract(2, "days").add(1, "month").format("DD MMM YYYY")
+    },
+    {
+        "invoiceNumber": 300497,
+        "nameOrder":"John Doe2",
+        "status": "Paid",
+        "progress": "Cooking",
+        "price": "233,42",
+        "issueDate": moment().subtract(3, "days").format("DD MMM YYYY"),
+        "dueDate": moment().subtract(3, "days").add(1, "month").format("DD MMM YYYY")
+    },
+    {
+        "invoiceNumber": 300496,
+        "status": "Due",
+        "subscription": "Gold Subscription Plan",
+        "price": "533,42",
+        "issueDate": moment().subtract(1, "day").subtract(1, "month").format("DD MMM YYYY"),
+        "dueDate": moment().subtract(1, "day").format("DD MMM YYYY")
+    },
+    {
+        "invoiceNumber": 300495,
+        "status": "Due",
+        "subscription": "Gold Subscription Plan",
+        "price": "533,42",
+        "issueDate": moment().subtract(3, "days").subtract(1, "month").format("DD MMM YYYY"),
+        "dueDate": moment().subtract(3, "days").format("DD MMM YYYY")
+    },
+    {
+        "invoiceNumber": 300494,
+        "status": "Due",
+        "subscription": "Flexible Subscription Plan",
+        "price": "233,42",
+        "issueDate": moment().subtract(4, "days").subtract(1, "month").format("DD MMM YYYY"),
+        "dueDate": moment().subtract(4, "days").format("DD MMM YYYY")
+    },
+    {
+        "invoiceNumber": 300493,
+        "status": "Canceled",
+        "subscription": "Gold Subscription Plan",
+        "price": "533,42",
+        "issueDate": moment().subtract(20, "days").subtract(1, "month").format("DD MMM YYYY"),
+        "dueDate": moment().subtract(20, "days").format("DD MMM YYYY")
+    },
+    {
+        "invoiceNumber": 300492,
+        "status": "Canceled",
+        "subscription": "Platinum Subscription Plan",
+        "price": "799,00",
+        "issueDate": moment().subtract(2, "months").format("DD MMM YYYY"),
+        "dueDate": moment().subtract(3, "months").format("DD MMM YYYY")
+    },
+    {
+        "invoiceNumber": 300491,
+        "status": "Paid",
+        "subscription": "Platinum Subscription Plan",
+        "price": "799,00",
+        "issueDate": moment().subtract(6, "days").format("DD MMM YYYY"),
+        "dueDate": moment().subtract(6, "days").add(1, "month").format("DD MMM YYYY")
+    }
+]
