cb2bcc0c5d9e74d48ab9f8ef2ac978bcb22f325b
JMJHOX <josemiguelaparicio507@gmail.com>
2022-11-16 22:19:41 -0500

    adding logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,33 +21,6 @@ export default (props) => {
               </Card.Link>
             </p>
           </Col>
-          <Col xs={12} lg={6}>
-            <ul className="list-inline list-group-flush list-group-borderless text-center text-xl-right mb-0">
-              <li className="list-inline-item px-0 px-sm-2">
-                <Card.Link href="https://themesberg.com/about" target="_blank">
-                  About
-                </Card.Link>
-              </li>
-              <li className="list-inline-item px-0 px-sm-2">
-                <Card.Link href="https://themesberg.com/themes" target="_blank">
-                  Themes
-                </Card.Link>
-              </li>
-              <li className="list-inline-item px-0 px-sm-2">
-                <Card.Link href="https://themesberg.com/blog" target="_blank">
-                  Blog
-                </Card.Link>
-              </li>
-              <li className="list-inline-item px-0 px-sm-2">
-                <Card.Link
-                  href="https://themesberg.com/contact"
-                  target="_blank"
-                >
-                  Contact
-                </Card.Link>
-              </li>
-            </ul>
-          </Col>
         </Row>
       </footer>
     </div>
